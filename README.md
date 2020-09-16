f701f66cd3e00c7ec08c4b2aea344ca3b00c8d99
jeanjacques <jeanjacques.strydom34@gmail.com>
2020-09-16 13:07:19 +0200

    Added readme file for github

---
+++ README.md
@@ -0,0 +1,3 @@
+#Hangouts Transcriber
+
+A small application to turn the Hangouts chat history (Hangouts.json) into a more readable text document
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+class Transcriber {
+    private static String filepath;
+
+    public Transcriber() {
+
+    }
+
+    private String readFile(String filepath) {
+        try {
+            return Files.readString(Path.of(filepath));
+        } catch (Exception ex) {
+            System.err.println("Failed to open file: " + filepath);
+            return null;
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new Transcriber();
     }
 }
